df82bae334dc5914fa62e12876e52ec923eb3184
Rory Holmes <rorythomas511@gmail.com>
2024-02-18 22:56:16 +0000

    Add rolling banner component

---
+++ app/components/RollingBanner.tsx
@@ -0,0 +1,23 @@
+'use client';
+
+const RollingBanner = () => {
+    const marqueeText =
+        'Billy Weaver Photo & Video • Billy Weaver Photo & Video • Billy Weaver Photo & Video • Billy Weaver Photo & Video • Billy Weaver Photo & Video • ';
+    // Create an array to determine the number of repetitions
+    const repetitions = Array.from({ length: 2 }, () => marqueeText);
+
+    return (
+        <div className="flex overflow-hidden gap-4 relative user-select-none">
+            {repetitions.map((content, index) => (
+                <h1
+                    key={index}
+                    className="scroll inter-font line-height[1.2] shrink-0 flex justify-around min-w-full gap-4 animate-scroll text-7xl mt-40 text-center font-semibold text-gray-100/80 z-30"
+                >
+                    {content}
+                </h1>
+            ))}
+        </div>
+    );
+};
+
+export default RollingBanner;

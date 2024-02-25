c286a2d25e562268d2569a62bfd5be901883407d
Rory Holmes <rorythomas511@gmail.com>
2024-02-25 14:54:22 +0000

    Decrease text size for contact page title on mobile

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -4,7 +4,7 @@ export default function Contact() {
     return (
         <div className="pt-28 p-5 max-w-screen-3xl flex flex-col md:flex-row md:gap-8">
             <div className="mb-10 w-full  md:mb-0 md:w-1/2 ">
-                <h2 className="mb-6 text-6xl inter-font font-semibold">
+                <h2 className="mb-6 text-4xl md:text-6xl inter-font font-semibold">
                     Contact me
                 </h2>
                 <p className="mb-6">
